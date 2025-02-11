d013e390a690e53198e34bec33e944befb778619
mmichiba <misakimichiba@gmail.com>
2025-02-11 15:10:18 +0800

    attempt so far

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+  # title = models.CharField(max_length=75)
+  # body = models.TextField()
+  # slug = models.SlugField()
+  # date = models.DateTimeField(auto_now_add=True)
+  # #Need to import Pillow for imagefield
+  # banner = models.ImageField(default='fallback.png', blank=True)
+
+  username = models.CharField(max_length=25)
+  password = models.CharField(max_length=25)
+
+  def __str__(self):
+    return self.username
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+
+def register(request):
+  users = User.objects.all()
+  return render(request, 'users/register.html', { 'users': users })
+
+# def post_page(request, slug):
+#   post = Post.objects.get(slug=slug)
+#   return render(request, 'posts/post_page.html', { 'post': post })
