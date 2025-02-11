baa3460518c16c576b24ec3baae09b52a90f2bca
mmichiba <misakimichiba@gmail.com>
2025-02-11 21:49:52 +0800

    created new post page that can only be accessed when user is logged in
    nav bar only displays register and login when user isn't logged in while user that is logged in can see new post and logout

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -5,5 +5,9 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts_list, name="list"),
+    path('new-post/', views.post_new, name="new-post"),
+
+    # need to put stuff before the line below (with the slug) 
+    # as it'll catch anything under it as a slug
     path('<slug:slug>', views.post_page, name="page"),
 ]
